6eb418f2d3b0a7fb9854c46845e044e70a1db608
abhijeet singh rajpurohit <abhijeetsinghbhainser@gmail.com>
2023-01-16 15:23:14 +0530

    backend done

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX',
+    })
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,     // the high risk it takes
+            max_tokens: 3000,   // 3000 word response
+            top_p: 1,
+            frequency_penalty: 0.5,   // no similar response
+            presence_penalty: 0
+    });
+
+    //sending the response    
+    res.status(200).send({
+        bot: response.data.choices[0].text
+    })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error})        
+    }
+})
+
+app.listen(5000, () => console.log('server is running on port http://localhost:5000'));
+
